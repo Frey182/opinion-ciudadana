47071490644c08e9a8da626a697f3ef118530629
Frey182 <alexander182fz@gmail.com>
2022-09-06 16:25:58 -0500

    contenedor creador, dentro un mediaobject de bootstrap

--- index.php
+++ index.php
@@ -18,6 +18,23 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js"></script>
 </head>
 <body>
-    
+    <div class="container p-5 w-75" style="height:100vh;">
+        <div class="container h-100">
+            <div class="media border p-3 rounded shadow-sm">
+                <img src="img_avatar3.png" alt="John Doe" class="mr-3 mt-3 rounded-circle" style="width:60px;">
+                <div class="media-body">
+                    <h4>John Doe <small><i>Posted on February 19, 2016</i></small></h4>
+                    <p>Lorem ipsum...</p>
+                    <div class="media p-3">
+                        <img src="img_avatar2.png" alt="Jane Doe" class="mr-3 mt-3 rounded-circle" style="width:45px;">
+                        <div class="media-body">
+                            <h4>Jane Doe <small><i>Posted on February 20 2016</i></small></h4>
+                            <p>Lorem ipsum...</p>
+                        </div>
+                    </div> 
+                </div>
+            </div>
+        </div>
+    </div>
 </body>
 </html>
