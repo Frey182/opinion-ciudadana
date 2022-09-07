0720d0cade46c145512ba0aa6c0e420602f3438e
Frey182 <alexander182fz@gmail.com>
2022-09-07 09:08:10 -0500

    se agrego el script de la base de datos

---
+++ model/bgoc.sql
@@ -0,0 +1,378 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 07-09-2022 a las 16:05:56
+-- Versión del servidor: 10.4.22-MariaDB
+-- Versión de PHP: 8.1.2
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `bgoc`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `barrio`
+--
+
+CREATE TABLE `barrio` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(40) NOT NULL,
+  `id_municipio` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `comentario`
+--
+
+CREATE TABLE `comentario` (
+  `id` int(11) NOT NULL,
+  `descripcion` varchar(280) NOT NULL,
+  `id_usu` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `discapacidad`
+--
+
+CREATE TABLE `discapacidad` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(40) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `etnia`
+--
+
+CREATE TABLE `etnia` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(40) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `municipio`
+--
+
+CREATE TABLE `municipio` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(40) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `nivel_educativo`
+--
+
+CREATE TABLE `nivel_educativo` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(25) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `sexo`
+--
+
+CREATE TABLE `sexo` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(15) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tipo_doc`
+--
+
+CREATE TABLE `tipo_doc` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tipo_usu`
+--
+
+CREATE TABLE `tipo_usu` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuario`
+--
+
+CREATE TABLE `usuario` (
+  `dni` int(11) NOT NULL,
+  `p_nombre` varchar(50) NOT NULL,
+  `s_nombre` varchar(50) NOT NULL,
+  `apellido_m` varchar(50) NOT NULL,
+  `apellido_p` varchar(50) NOT NULL,
+  `nacimiento` varchar(15) NOT NULL,
+  `telefono` int(11) NOT NULL,
+  `correo` varchar(50) NOT NULL,
+  `direccion` varchar(30) NOT NULL,
+  `internet` int(2) NOT NULL,
+  `acesso_dispositivo` varchar(2) NOT NULL,
+  `dispositivo` varchar(100) NOT NULL,
+  `fktipo_doc` int(11) NOT NULL,
+  `fktipo_usu` int(11) NOT NULL,
+  `fketnia` int(11) NOT NULL,
+  `fksexo` int(11) NOT NULL,
+  `fknvledu` int(11) NOT NULL,
+  `fkbarrio` int(11) NOT NULL,
+  `fkmunicipio` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuario_discapacidad`
+--
+
+CREATE TABLE `usuario_discapacidad` (
+  `id` int(11) NOT NULL,
+  `id_discapacidad` int(11) NOT NULL,
+  `id_usuario` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `barrio`
+--
+ALTER TABLE `barrio`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_municipio` (`id_municipio`);
+
+--
+-- Indices de la tabla `comentario`
+--
+ALTER TABLE `comentario`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_usu` (`id_usu`);
+
+--
+-- Indices de la tabla `discapacidad`
+--
+ALTER TABLE `discapacidad`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `etnia`
+--
+ALTER TABLE `etnia`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `municipio`
+--
+ALTER TABLE `municipio`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `nivel_educativo`
+--
+ALTER TABLE `nivel_educativo`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `sexo`
+--
+ALTER TABLE `sexo`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `tipo_doc`
+--
+ALTER TABLE `tipo_doc`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `tipo_usu`
+--
+ALTER TABLE `tipo_usu`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `usuario`
+--
+ALTER TABLE `usuario`
+  ADD PRIMARY KEY (`dni`),
+  ADD KEY `fktipo_doc` (`fktipo_doc`),
+  ADD KEY `fktipo_usu` (`fktipo_usu`),
+  ADD KEY `fketnia` (`fketnia`),
+  ADD KEY `fksexo` (`fksexo`),
+  ADD KEY `fknvledu` (`fknvledu`),
+  ADD KEY `fkbarrio` (`fkbarrio`),
+  ADD KEY `fkmunicipio` (`fkmunicipio`);
+
+--
+-- Indices de la tabla `usuario_discapacidad`
+--
+ALTER TABLE `usuario_discapacidad`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_discapacidad` (`id_discapacidad`),
+  ADD KEY `id_usuario` (`id_usuario`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `barrio`
+--
+ALTER TABLE `barrio`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `comentario`
+--
+ALTER TABLE `comentario`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `discapacidad`
+--
+ALTER TABLE `discapacidad`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `etnia`
+--
+ALTER TABLE `etnia`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `municipio`
+--
+ALTER TABLE `municipio`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `nivel_educativo`
+--
+ALTER TABLE `nivel_educativo`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `sexo`
+--
+ALTER TABLE `sexo`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `tipo_doc`
+--
+ALTER TABLE `tipo_doc`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `tipo_usu`
+--
+ALTER TABLE `tipo_usu`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `usuario_discapacidad`
+--
+ALTER TABLE `usuario_discapacidad`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `barrio`
+--
+ALTER TABLE `barrio`
+  ADD CONSTRAINT `barrio_ibfk_1` FOREIGN KEY (`id`) REFERENCES `usuario` (`fkbarrio`) ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `discapacidad`
+--
+ALTER TABLE `discapacidad`
+  ADD CONSTRAINT `discapacidad_ibfk_1` FOREIGN KEY (`id`) REFERENCES `usuario_discapacidad` (`id_discapacidad`);
+
+--
+-- Filtros para la tabla `etnia`
+--
+ALTER TABLE `etnia`
+  ADD CONSTRAINT `etnia_ibfk_1` FOREIGN KEY (`id`) REFERENCES `usuario` (`fketnia`) ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `municipio`
+--
+ALTER TABLE `municipio`
+  ADD CONSTRAINT `municipio_ibfk_1` FOREIGN KEY (`id`) REFERENCES `barrio` (`id_municipio`) ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `nivel_educativo`
+--
+ALTER TABLE `nivel_educativo`
+  ADD CONSTRAINT `nivel_educativo_ibfk_1` FOREIGN KEY (`id`) REFERENCES `usuario` (`fknvledu`) ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `sexo`
+--
+ALTER TABLE `sexo`
+  ADD CONSTRAINT `sexo_ibfk_1` FOREIGN KEY (`id`) REFERENCES `usuario` (`fksexo`) ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `tipo_doc`
+--
+ALTER TABLE `tipo_doc`
+  ADD CONSTRAINT `tipo_doc_ibfk_1` FOREIGN KEY (`id`) REFERENCES `usuario` (`fktipo_doc`) ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `tipo_usu`
+--
+ALTER TABLE `tipo_usu`
+  ADD CONSTRAINT `tipo_usu_ibfk_1` FOREIGN KEY (`id`) REFERENCES `usuario` (`fktipo_usu`) ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `usuario`
+--
+ALTER TABLE `usuario`
+  ADD CONSTRAINT `usuario_ibfk_1` FOREIGN KEY (`dni`) REFERENCES `comentario` (`id_usu`) ON UPDATE CASCADE,
+  ADD CONSTRAINT `usuario_ibfk_2` FOREIGN KEY (`fkmunicipio`) REFERENCES `municipio` (`id`) ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `usuario_discapacidad`
+--
+ALTER TABLE `usuario_discapacidad`
+  ADD CONSTRAINT `usuario_discapacidad_ibfk_1` FOREIGN KEY (`id_usuario`) REFERENCES `usuario` (`dni`) ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
