82cdebfbf337d6c25c4262a1a821cf84b984d483
DuvanBV <Dbolanoo07@gmail.com>
2022-09-07 12:55:22 -0500

    se edito el campo de las publicaciones

--- index.php
+++ index.php
@@ -1,76 +1,31 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Opinión ciudadana</title>
-    <!-- Latest compiled and minified CSS -->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css">
-
-    <!-- jQuery library -->
-    <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.0/dist/jquery.slim.min.js"></script>
-
-    <!-- Popper JS -->
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
-
-    <!-- Latest compiled JavaScript -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js"></script>
-</head>
-<body>
-    <nav class="navbar navbar-expand-lg navbar-light bg-warning">
-        <a class="navbar-brand" href="#">Opinion Ciudadana</a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav mr-auto">
-                <li class="nav-item active">
-                    <a class="nav-link" href="#">Inicio <span class="sr-only">(current)</span></a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Link</a>
-                </li>
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-expanded="false">
-                    Dropdown
-                    </a>
-                    <div class="dropdown-menu">
-                    <a class="dropdown-item" href="#">Action</a>
-                    <a class="dropdown-item" href="#">Another action</a>
-                    <div class="dropdown-divider"></div>
-                    <a class="dropdown-item" href="#">Something else here</a>
+<?php include "view/header.php"?>
+        <div class="contepublicacion">
+            <div class="container h-100">
+                <div class="media border p-3 rounded shadow-sm bg-warning">
+                    <div class="conteimguser">
+                        <img src="view/img/logogobierno.png" alt="John Doe" class="mr-3 mt-1 rounded-circle" style="width:60px;">
                     </div>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link disabled">Disabled</a>
-                </li>
-            </ul>
-            <form class="form-inline my-2 my-lg-0">
-                <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-                 
-                <button type="submit" class="btn btn-outline-light font-weight-bold">Buscar</button>
-            </form>
-        </div>
-    </nav>
-    <div class="container p-5 w-75" style="height:100vh;">
-        <div class="container h-100">
-            <div class="media border p-3 rounded shadow-sm">
-                <img src="img_avatar3.png" alt="John Doe" class="mr-3 mt-3 rounded-circle" style="width:60px;">
-                <div class="media-body">
-                    <h4>John Doe <small><i>Posted on February 19, 2016</i></small></h4>
-                    <p>Lorem ipsum...</p>
-                    <div class="media p-3">
-                        <img src="img_avatar2.png" alt="Jane Doe" class="mr-3 mt-3 rounded-circle" style="width:45px;">
-                        <div class="media-body">
-                            <h4>Jane Doe <small><i>Posted on February 20 2016</i></small></h4>
+                    <div class="media-body ">
+                        <h4>Duvan Bolaño <small><i>Posted on February 19, 2016</i></small></h4>
+                        <p>Duvan@gmail.com</p>
+                    
+                    </div>
+                </div>
+                <div class="media p-3 bg-white ">
+                        <img src="" alt="" class="" style="">
+                        <div class="media-body text-align-center">
+                            <h4>Nueva publicacion........</h4>
                             <p>Lorem ipsum...</p>
                         </div>
-                    </div> 
+                </div>
+                <div class="media bg-warning p-2 rounded-bottom">
+                    <div class="input-group mb-1">
+                    <textarea class="form-control" aria-label="With textarea"></textarea>
+                    <div class="input-group-append">
+                        <button class="btn btn-outline-secondary" type="button" id="button-addon2">Button</button>
+                    </div>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
-</body>
-</html>
+<?php include "view/footer.php"?>
