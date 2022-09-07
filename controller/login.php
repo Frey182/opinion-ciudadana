d1f0447e11d2131fe4ac1d1618f0a644457798fd
Frey182 <alexander182fz@gmail.com>
2022-09-07 09:21:59 -0500

    se agrego el input dni

--- controller/login.php
+++ controller/login.php
@@ -15,6 +15,7 @@ if ($_POST) {
             // para registrarse
             case 'registrarse':
                 // guardamos los names de los inputs del formulario de registro
+                $dni = $_POST['dni'];
                 $p_nombre = $_POST['p_nombre'];
                 $s_nombre = $_POST['s_nombre'];
                 $apellido_m = $_POST['apellido_m'];
@@ -35,7 +36,7 @@ if ($_POST) {
                 $fkmunicipio = $_POST['fkmunicipio'];
 
                 // hacemos la insercción en la tabla
-                $sql = "INSERT INTO `usuario` (`dni`, `p_nombre`, `s_nombre`, `apellido_m`, `apellido_p`, `nacimiento`, `telefono`, `correo`, `direccion`, `internet`, `acesso_dispositivo`, `dispositivo`, `fktipo_doc`, `fktipo_usu`, `fketnia`, `fksexo`, `fknvledu`, `fkbarrio`, `fkmunicipio`) VALUES (NULL, '$p_nombre ', '$s_nombre', '$apellido_m', '$apellido_p', '$nacimiento', '$telefono', '$correo', '$direccion', '$internet ', '$acesso_dispositivo', '$dispositivo', '$fktipo_doc', '$fktipo_usu', '$fketnia', '$fksexo', '$fknvledu', '$fkbarrio', '$fkmunicipio')";
+                $sql = "INSERT INTO `usuario` (`dni`, `p_nombre`, `s_nombre`, `apellido_m`, `apellido_p`, `nacimiento`, `telefono`, `correo`, `direccion`, `internet`, `acesso_dispositivo`, `dispositivo`, `fktipo_doc`, `fktipo_usu`, `fketnia`, `fksexo`, `fknvledu`, `fkbarrio`, `fkmunicipio`) VALUES ('$dni', '$p_nombre ', '$s_nombre', '$apellido_m', '$apellido_p', '$nacimiento', '$telefono', '$correo', '$direccion', '$internet ', '$acesso_dispositivo', '$dispositivo', '$fktipo_doc', '$fktipo_usu', '$fketnia', '$fksexo', '$fknvledu', '$fkbarrio', '$fkmunicipio')";
                 // llamamos al método ejecutar de conexion
                 $objConexion->ejecutar($sql);
                 break;
