f3e41e9936c903128f151f43689885848a00f4de
Frey182 <alexander182fz@gmail.com>
2022-09-07 11:12:08 -0500

    se seleccionar tablas

---
+++ model/seleccion.php
@@ -0,0 +1,23 @@
+<?php
+
+$objConexion = new conexion;
+
+$etnias = "SELECT * FROM `etnia`";
+$etnias = $objConexion->consultar($etnias);
+
+$municipios = "SELECT * FROM `municipio`";
+$municipios = $objConexion->consultar($municipios);
+
+$sexos = "SELECT * FROM `sexo`";
+$sexos = $objConexion->consultar($sexos);
+
+$tipo_docs = "SELECT * FROM `tipo_doc`";
+$tipo_docs = $objConexion->consultar($tipo_docs);
+
+$discapacidades = "SELECT * FROM `discapacidad`";
+$discapacidades = $objConexion->consultar($discapacidades);
+
+$niveles_educativos = "SELECT * FROM `nivel_educativo`";
+$niveles_educativos = $objConexion->consultar($niveles_educativos);
+
+?>
