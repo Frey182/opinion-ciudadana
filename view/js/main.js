c73f67743862758b8b9dd0cc605c31df85ef7e41
Frey182 <alexander182fz@gmail.com>
2022-09-07 12:15:10 -0500

    codigo para retroceder el slider de register

--- view/js/main.js
+++ view/js/main.js
@@ -4,7 +4,9 @@ const slider3 = document.getElementById('slider3');
 
 const btnSlider1 = document.getElementById('btnSlider1');
 const btnSlider2 = document.getElementById('btnSlider2');
-const btnSlider3 = document.getElementById('btnSlider3');
+
+const btnSliderBack1 = document.getElementById('btnSliderBack1');
+const btnSliderBack2 = document.getElementById('btnSliderBack2');
 
 btnSlider1.addEventListener('click', function() {
 
@@ -17,5 +19,19 @@ btnSlider2.addEventListener('click', function() {
 
     slider2.classList.add('d-none');
     slider3.classList.remove('d-none');
+
+});
+
+btnSliderBack1.addEventListener('click', function() {
+
+    slider2.classList.add('d-none');
+    slider1.classList.remove('d-none');
+
+});
+
+btnSliderBack2.addEventListener('click', function() {
+
+    slider3.classList.add('d-none');
+    slider2.classList.remove('d-none');
 
 });
