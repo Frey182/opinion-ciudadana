2b91c0bb18b6a2a85ec6d1401b767e1921fb0ce1
DuvanBV <Dbolanoo07@gmail.com>
2022-09-08 07:46:36 -0500

    se edito el cuadro de la publicacion y el barnav

--- index.php
+++ index.php
@@ -2,28 +2,50 @@
 <link rel="stylesheet" href="view/css/estilos.css">
         <div class="contepublicacion">
             <div class="container h-100">
-                <div class="media border p-3 rounded shadow-sm bg-warning">
-                    <div class="conteimguser">
-                        <img src="view/img/logogobierno.png" alt="John Doe" class="mr-3 mt-1 rounded-circle" style="width:60px;">
+                <div class="card">
+                    <div class="card-header d-flex border p-3 rounded shadow-sm bg-warning">
+                        <div class="conteimguser">
+                            <img src="view/img/logogobierno.png" alt="John Doe" class="mr-3 mt-1 rounded-circle" style="width:60px;">
+                        </div>
+                        <div class="contenameusu">
+                            <h4>Duvan Bolaño <small><i>Posted on February 19, 2016</i></small></h4>
+                            <p>Duvan@gmail.com</p>
+                        </div>
                     </div>
-                    <div class="media-body ">
-                        <h4>Duvan Bolaño <small><i>Posted on February 19, 2016</i></small></h4>
-                        <p>Duvan@gmail.com</p>
-                    
-                    </div>
-                </div>
-                <div class="media p-3 bg-white ">
-                        <img src="" alt="" class="" style="">
+                    <div class="card-body p-3 bg-white">
+                        
                         <div class="media-body text-align-center">
                             <h4 class="test">Nueva publicacion........</h4>
                             <p>Lorem ipsum...</p>
+                            <img src="" alt="" class="" style="">
                         </div>
+                    </div>
+                    <div class="card-footer text-muted">
+                        <div class="media border p-3">
+                            <img src="view/img/logogobierno.png" alt="John Doe" class="mr-3 mt-1 rounded-circle" style="width:60px;">
+                            <div class="media-body">
+                                <h4>John Doe <small><i>Posted on February 19, 2016</i></small></h4>
+                                <p>Lorem ipsum...</p>
+                                <div class="media p-3">
+                                <img src="view/img/logogobierno.png" alt="Jane Doe" class="mr-3 mt-1 rounded-circle" style="width:45px;">
+                                <div class="media-body">
+                                    <h4>Jane Doe <small><i>Posted on February 20 2016</i></small></h4>
+                                    <p>Lorem ipsum...</p>
+                                </div>
+                                </div> 
+                            </div>
+                        </div>   
+                    </div>
                 </div>
-                <div class="media bg-warning p-2 rounded-bottom">
+                    
+                <div class="media ">
+                   
+                </div>
+                <div class="media p-2 rounded-bottom" style="background-color: white;">
                     <div class="input-group mb-1">
-                    <textarea class="form-control" row="1" col="50" aria-label="With textarea"></textarea>
+                    <textarea class="form-control bg-light" row="1" col="50" aria-label="With textarea"></textarea>
                     <div class="input-group-append">
-                        <button class="btn btn-outline-success" type="button" id="button-addon2">Comentar</button>
+                        <button class="btn btn-warning" type="button" id="button-addon2">Comentar</button>
                     </div>
                     </div>
                 </div>
