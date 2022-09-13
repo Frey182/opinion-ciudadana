d2918d70dee72af6946859c6e0323ecafa7ea8fd
frey182 <alexander182fz@gmail.com>
2022-09-13 11:38:32 -0500

    comentarios

--- index.php
+++ index.php
@@ -49,7 +49,59 @@
                         </div>
                     </div>
                 <?php } ?>
-               
+                <!-- PARA PUBLICACIONES -->
+                <?php foreach ($temas as $tema) { ?>
+                    <div class="card mb-3">
+                        <div class="publicacion" id="<?php echo $tema['id']; ?>">
+                            <div class="card-header d-flex border p-3 rounded shadow-sm bg-warning">
+                                <div class="conteimguser">
+                                    <img src="view/img/logogobierno.png" alt="John Doe" class="mr-3 mt-1 rounded-circle" style="width:60px;">
+                                </div>
+                                <div class="contenameusu">
+                                    <h4><?php echo $tema['nombre']; ?> <small><i>Publicado <?php echo $tema['fecha']; ?></i></small></h4>
+                                    <p>por "<?php echo $tema['correo']; ?>"</p>
+                                </div>
+                            </div>
+                            <div class="card-body p-3 bg-white">
+                                
+                                <div class="media-body text-align-center">
+                                    <p><?php echo $tema['descripcion']; ?></p>
+                                    <img src="" alt="" class="" style="">
+                                </div>
+                            </div>
+                            <div class="card-footer text-muted">
+                                <?php foreach ($comentarios as $comentario) { ?>
+                                    <div class="media border p-3 bg-light comentario" id="<?php echo $comentario['id'] ?>">
+                                        <img src="view/img/logogobierno.png" alt="John Doe" class="mr-3 mt-1 rounded-circle" style="width:60px;">
+                                        <div class="media-body">
+                                            <h4>John Doe <small><i>Posted on February 19, 2016</i></small></h4>
+                                            <p><?php echo $comentario['descripcion']; ?></p>
+                                            <div class="media p-3">
+                                            <img src="view/img/logogobierno.png" alt="Jane Doe" class="mr-3 mt-1 rounded-circle" style="width:45px;">
+                                            <div class="media-body">
+                                                <h4>Jane Doe <small><i>Posted on February 20 2016</i></small></h4>
+                                                <p>Lorem ipsum...</p>
+                                            </div>
+                                            </div> 
+                                        </div>
+                                    </div>   
+                                <?php } ?>
+                            </div>
+                            <div class="media bg-white p-2 rounded-bottom">
+                                <form action="index.php" method="post" class="w-100">
+                                    <div class="input-group mb-1 texto">
+                                        <input type="hidden" name="tema" value="<?php echo $tema['id']; ?>">
+                                        <textarea name="descripcion" class="form-control texto bg-light" row="1" col="50" aria-label="With textarea"></textarea>
+                                        <div class="input-group-append">
+                                            <button name="comentar" value="comentar" class="btn btn-outline-warning font-weight-bold" type="submit" id="button-addon2">Comentar</button>
+                                        </div>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                    </div> 
+                <?php } ?>
             </div>
         </div>
 <?php include "view/footer.php"?>
+<script src="view/js/comentarios.js"></script>
--- model/seleccion.php
+++ model/seleccion.php
@@ -29,4 +29,21 @@ $temas = $objConexion->consultar($temas);
 $comentarios = "SELECT * FROM comentario;";
 $comentarios = $objConexion->consultar($comentarios);
 
+/* 
+SELECT 
+tema.id AS tema_id, 
+tema.nombre AS tema_nombre, 
+tema.descripcion AS tema_descripcion, 
+tema.imagen AS tema_imagen,
+tema.fecha AS tema_fecha,
+tema.fkusuario AS tema_fkusuario, 
+usuario.dni AS usuario_dni, 
+usuario.correo AS usuario_correo, 
+comentario.id AS comentario_id, 
+comentario.descripcion AS comentario_descripcion, 
+comentario.id_usu AS comentario_idtema, 
+comentario.id_usu AS comentario_idusu 
+FROM tema INNER JOIN usuario ON tema.fkusuario = usuario.dni INNER JOIN comentario ON comentario.id_usu = usuario.dni ORDER BY tema.id DESC;
+*/
+
 ?>
