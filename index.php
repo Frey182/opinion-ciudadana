e3e9629fbdd7e9888a5949e86e1241eff3d2b1f9
Mariibrch <mdbracho1@misena.edu.co>
2022-09-13 11:34:28 -0500

    se agrego el modal para el tipo de publico

--- index.php
+++ index.php
@@ -20,6 +20,122 @@
                                         <textarea name="descripcion" class="form-control bg-light" placeholder="Escribe tu contenido" id="exampleFormControlTextarea1" rows="3"></textarea>
                                     </div>
                                     <div class="d-flex float-right">
+                                        <!-- Button trigger modal -->
+                                            <button type="button" class="btn btn-light" data-toggle="modal" data-target="#exampleModalCenter">
+                                           ¿Para quien?
+                                            </button>
+
+                                            <!-- Modal -->
+                                            <div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+                                                <div class="modal-dialog modal-dialog-centered" role="document">
+                                                   <form action="">
+                                                        <div class="modal-content">
+                                                            <div class="modal-header">
+                                                                <h5 class="modal-title" id="exampleModalLongTitle">Elige para que publico sera tu publicacion</h5>
+                                                            </div>
+                                                            <div class="modal-body">
+                                                                <div class="conte">
+                                                                    <div class="form-group mr-2 my-2 my-lg-2">
+                                                                            <select class="form-control" id="exampleFormControlSelect2" name="publico">
+                                                                            <option>Publico</option>
+                                                                            <option>Todos</option>
+                                                                            <?php foreach ($sexos as $sexo) { ?>
+                                                                            <option value="<?php echo $sexo['id'] ?>"><?php echo $sexo['nombre'] ?></option>
+                                                                            <?php } ?>
+                                                                            </select>
+                                                                        </div>
+                                                                        <div class="form-group mr-2 my-2 my-lg-2">
+                                                                            <select class="form-control" id="exampleFormControlSelect2" name="edad">
+                                                                            <!-- <i class="fa-solid fa-users"> Publico</i> -->
+                                                                            <option >Edad</option>
+                                                                            <option>Todas</option>
+                                                                            <option>...</option>
+                                                                            <option>...</option>
+                                                                            <option>...</option>
+                                                                            </select>
+                                                                        </div>
+                                                                        <div class="form-group mr-2 my-2 my-lg-2">
+                                                                            <select class="form-control" id="exampleFormControlSelect2" name="municipio">
+                                                                            <!-- <i class="fa-solid fa-users"> Publico</i> -->
+                                                                            <option >Municipio</option>
+                                                                            <option>Todos</option>
+                                                                            <?php foreach ($municipios as $municipio) { ?>
+                                                                            <option value="<?php echo $municipio['id'] ?>"><?php echo $municipio['nombre'] ?></option>
+                                                                            <?php } ?>
+                                                                            </select>
+                                                                        </div>
+                                                                        <div class="form-group mr-2 my-2 my-lg-2">
+                                                                            <select class="form-control" id="exampleFormControlSelect2" name="barrio">
+                                                                            <!-- <i class="fa-solid fa-users"> Publico</i> -->
+                                                                            <option >Barrio/Vereda</option>
+                                                                            <option>Todos</option>
+                                                                            <?php foreach ($barrios as $barrio) { ?>
+                                                                            <option value="<?php echo $barrio['id'] ?>"><?php echo $barrio['nombre'] ?></option>
+                                                                            <?php } ?>
+                                                                            </select>
+                                                                        </div>
+                                                                    <div class="form-group mr-2 my-2 my-lg-2">
+                                                                            <select class="form-control" id="exampleFormControlSelect2" name="etnia">
+                                                                            <!-- <i class="fa-solid fa-users"> Publico</i> -->
+                                                                            <option >Etnia</option>
+                                                                            <option>Todas</option>
+                                                                            <?php foreach ($etnias as $etnia) { ?>
+                                                                            <option value="<?php echo $etnia['id'] ?>"><?php echo $etnia['nombre'] ?></option>
+                                                                            <?php } ?>
+                                                                            </select>
+                                                                        </div>
+                                                                        <div class="form-group mr-2 my-2 my-lg-2">
+                                                                            <select class="form-control" id="exampleFormControlSelect2" name="discapacidad">
+                                                                            <!-- <i class="fa-solid fa-users"> Publico</i> -->
+                                                                            <option >Discapacidad</option>
+                                                                            <option>Todas</option>
+                                                                            <?php foreach ($discapacidades as $discapacidad) { ?>
+                                                                            <option value="<?php echo $discapacidad['id'] ?>"><?php echo $discapacidad['nombre'] ?></option>
+                                                                            <?php } ?>
+                                                                            </select>
+                                                                        </div>
+                                                                        <div class="form-group mr-2 my-2 my-lg-2">
+                                                                            <select class="form-control" id="exampleFormControlSelect2" name="estrato">
+                                                                            <!-- <i class="fa-solid fa-users"> Publico</i> -->
+                                                                            <option >Estrato</option>
+                                                                            <option>Todas</option>
+                                                                            <option value="1">1</option>
+                                                                            <option value="2">2</option>
+                                                                            <option value="3">3</option>
+                                                                            <option value="4">4</option> 
+                                                                            <option value="5">5</option>
+                                                                            <option value="6">6</option>
+                                                                            </select>
+                                                                        </div>
+                                                                        <div class="form-group mr-2 my-2 my-lg-2">
+                                                                            <select class="form-control" id="exampleFormControlSelect2" name="nvledu">
+                                                                            <!-- <i class="fa-solid fa-users"> Publico</i> -->
+                                                                            <option >Nivel educativo</option>
+                                                                            <option>Todas</option>
+                                                                            <?php foreach ($niveles_educativos as $nivel_educativo) { ?>
+                                                                            <option value="<?php echo $nivel_educativo['id'] ?>"><?php echo $nivel_educativo['nombre'] ?></option>
+                                                                            <?php } ?>
+                                                                            </select>
+                                                                        </div>
+                                                                        <div class="form-group mr-2 my-2 my-lg-2">
+                                                                            <select class="form-control" id="exampleFormControlSelect2" name="regimen">
+                                                                            <!-- <i class="fa-solid fa-users"> Publico</i> -->
+                                                                            <option >Regimen</option>
+                                                                            <option>Todas</option>
+                                                                            <option value="1">Contributivo</option>
+                                                                            <option value="1">Subcidiado</option>
+                                                                            </select>
+                                                                        </div>
+                                                                    </div>
+                                                                </div>
+                                                            <div class="modal-footer">
+                                                                <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancelar</button>
+                                                                <button type="submit" class="btn btn-warning" name="Para">Aceptar</button>
+                                                            </div>
+                                                        </div>
+                                                   </form>
+                                                </div>
+                                            </div>
                                             <div class="form-group mr-2 my-2 my-lg-2">
                                                 <select class="form-control" id="exampleFormControlSelect1" name="tipo_publi">
                                                 <option disable>Tipo de publicacion</option>
@@ -28,17 +144,6 @@
                                                 <option>Opinion</option>
                                                 <option value="publicacion">Publicacion</option>
                                                 </select>
-                                            </div>
-                                            <div class="form-group mr-2 my-2 my-lg-2">
-                                                <select class="form-control" id="exampleFormControlSelect2">
-                                                <!-- <i class="fa-solid fa-users"> Publico</i> -->
-                                                <option>Publico</option>
-                                                <option>Hombres</option>
-                                                <option>Mujeres</option>
-                                                <option>14 - 19 años</option>
-                                                <option>5</option>
-                                                </select>
-                                                
                                             </div>
                                         <button class="btn btn-light border-secondary rounded-circle mr-2" style="height: 50px; width: 50px;"><i class="fa-regular fa-image"></i></button>
                                         <button name="enviar" type="submit" class="btn btn-success rounded-circle" style="height: 50px; width: 50px;"><i class="fa-solid fa-arrow-up-from-bracket" ></i></button>
@@ -49,7 +154,56 @@
                         </div>
                     </div>
                 <?php } ?>
-               
+                <?php foreach ($temas as $tema) { ?>
+                    <div class="card mb-3">
+                        <div class="card-header d-flex border p-3 rounded shadow-sm bg-warning">
+                            <div class="conteimguser">
+                                <img src="view/img/logogobierno.png" alt="John Doe" class="mr-3 mt-1 rounded-circle" style="width:60px;">
+                            </div>
+                            <div class="contenameusu">
+                                <h4><?php echo $tema['nombre']; ?> <small><i>Publicado <?php echo $tema['fecha']; ?></i></small></h4>
+                                <p>por "<?php echo $tema['correo']; ?>"</p>
+                            </div>
+                        </div>
+                        <div class="card-body p-3 bg-white">
+                            
+                            <div class="media-body text-align-center">
+                                <p><?php echo $tema['descripcion']; ?></p>
+                                <img src="" alt="" class="" style="">
+                            </div>
+                        </div>
+                    <?php foreach ($comentarios as $comentario) { ?>
+                        <div class="card-footer text-muted">
+                            <div class="media border p-3 bg-light">
+                                <img src="view/img/logogobierno.png" alt="John Doe" class="mr-3 mt-1 rounded-circle" style="width:60px;">
+                                <div class="media-body">
+                                    <h4>John Doe <small><i>Posted on February 19, 2016</i></small></h4>
+                                    <p><?php echo $comentario['descripcion']; ?></p>
+                                    <div class="media p-3">
+                                    <img src="view/img/logogobierno.png" alt="Jane Doe" class="mr-3 mt-1 rounded-circle" style="width:45px;">
+                                    <div class="media-body">
+                                        <h4>Jane Doe <small><i>Posted on February 20 2016</i></small></h4>
+                                        <p>Lorem ipsum...</p>
+                                    </div>
+                                    </div> 
+                                </div>
+                            </div>   
+                        </div>
+                    <?php } ?>
+                        <div class="media bg-white p-2 rounded-bottom">
+                            <form action="index.php" method="post" class="w-100">
+                                <div class="input-group mb-1 texto">
+                                    <input type="hidden" name="tema" value="<?php echo $tema['id']; ?>">
+                                    <textarea name="descripcion" class="form-control texto bg-light" row="1" col="50" aria-label="With textarea"></textarea>
+                                    <div class="input-group-append">
+                                        <button name="comentar" value="comentar" class="btn btn-outline-warning font-weight-bold" type="submit" id="button-addon2">Comentar</button>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                    </div> 
+                <?php } ?>
+
             </div>
         </div>
 <?php include "view/footer.php"?>
