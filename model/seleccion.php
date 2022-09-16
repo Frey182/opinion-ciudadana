1af46a2f007eef3919dfc7095dc417e813b92f38
Frey <alexander182fz@gmail.com>
2022-09-16 07:47:06 -0500

    se seleccionan los temas que sean aptos para la edad del usuario

--- model/seleccion.php
+++ model/seleccion.php
@@ -31,6 +31,15 @@ $opcion_encuestas = $objConexion->consultar($opcion_encuestas);
 
 $condiciones = "SELECT * FROM condicion;";
 $condiciones = $objConexion->consultar($condiciones);
+
+if (isset($_SESSION['edad'])) {
+    $temas = "SELECT * FROM tema 
+    INNER JOIN usuario ON tema.fkusuario = usuario.dni 
+    INNER JOIN condicion ON tema.id = condicion.id_tema
+    WHERE condicion.edad =".$_SESSION['edad'];
+    $temas = $objConexion->consultar($temas);
+}
+
 /* 
 SELECT 
 tema.id AS tema_id, 
