b67a51635d8ffb6e6758a8a8e2d92b701f0d5b68
frey182 <alexander182fz@gmail.com>
2022-09-20 19:23:00 -0500

    ya se puede responder encuestas

--- controller/resencuesta.php
+++ controller/resencuesta.php
@@ -2,17 +2,19 @@
 
 if ($_POST) {
 
-    if  ((isset($_POST['enviar'])) {
+    if  (isset($_POST['enviar'])) {
 
-        if($_POST['enviar'] == 'resenviar'))
-        $objConexion = new Conexion();
+        if($_POST['enviar'] == 'resencuesta') {
+            
+            $id_encuesta = $_POST['idtema'];
+            $id_usuario = $_POST['usuario'];
+            $opcion = $_POST['opcion'];
+            
+            $objConexion = new Conexion();
+            $sql = "INSERT INTO `encuesta_usuario` (`id`, `id_usuario`, `id_encuesta`, `respuestas`) VALUES (NULL, '$id_usuario', '$id_encuesta', '$opcion')";
+            $objConexion->ejecutar($sql);
 
-        $id_encuesta = $_POST['idtema'];
-        $id_usuario = $_SESSION['usuario'];
-        $opcion = $_POST['opcion'];
-
-        $sql = "INSERT INTO `encuesta_usuario` (`id`, `id_usuario`, `id_encuesta`, `respuestas`) VALUES (NULL, $id_usuario, '$id_encuesta', $opcion)";
-        $objConexion->ejecutar($sql);
+        }
 
     }
 
