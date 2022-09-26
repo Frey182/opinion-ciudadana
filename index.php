c7ca2df3bcfab6e72c8b0338452f2625ba551c94
frey182 <alexander182fz@gmail.com>
2022-09-25 21:36:45 -0500

    se comprueba que por cada tema, exista la clave id_tema

--- index.php
+++ index.php
@@ -205,105 +205,107 @@ if (empty($_SESSION)) {
 
                 <!-- PARA PUBLICACIONES -->
                 <?php foreach ($temas as $tema) { ?>
-                    <?php 
-                        $encuestas = "SELECT * FROM encuesta_usuario WHERE id_encuesta = ".$tema['id_tema']." AND id_usuario = ". $_SESSION['dni'];
-                        $encuestas = $objConexion->consultar($encuestas);
-                    ?>
-                    <div class="card mb-3 publicacion shadow-sm" id="<?php echo $tema['id']; ?>">
-                        <div>
-                            <div class="card-header d-flex border p-3 rounded shadow-sm" style="background-color:  #449aff;">
-                                <div class="conteimguser">
-                                    <img src="view/img/logogobierno.png" alt="John Doe" class="mr-3 mt-1 rounded-circle" style="width:60px;">
+                    <?php if (isset($tema['id_tema'])) { ?>
+                        <?php 
+                            $encuestas = "SELECT * FROM encuesta_usuario WHERE id_encuesta = ".$tema['id_tema']." AND id_usuario = ". $_SESSION['dni'];
+                            $encuestas = $objConexion->consultar($encuestas);
+                        ?>
+                        <div class="card mb-3 publicacion shadow-sm" id="<?php echo $tema['id']; ?>">
+                            <div>
+                                <div class="card-header d-flex border p-3 rounded shadow-sm" style="background-color:  #449aff;">
+                                    <div class="conteimguser">
+                                        <img src="view/img/logogobierno.png" alt="John Doe" class="mr-3 mt-1 rounded-circle" style="width:60px;">
+                                    </div>
+                                    <div class="contenameusu">
+                                        <h4><?php echo $tema['nombre']; ?> <small><i>Publicado <?php echo $tema['fecha']; ?></i></small></h4>
+                                        <p>por "<?php echo $tema['correo']; ?>"</p>
+                                    </div>
                                 </div>
-                                <div class="contenameusu">
-                                    <h4><?php echo $tema['nombre']; ?> <small><i>Publicado <?php echo $tema['fecha']; ?></i></small></h4>
-                                    <p>por "<?php echo $tema['correo']; ?>"</p>
+                                <div class="card-body p-3 bg-white">
+                                    
+                                    <div class="media-body text-align-center">
+                                        <p><?php echo $tema['descripcion']; ?></p>
+                                        <img src="" alt="" class="" style="">
+                                    </div>
                                 </div>
-                            </div>
-                            <div class="card-body p-3 bg-white">
-                                
-                                <div class="media-body text-align-center">
-                                    <p><?php echo $tema['descripcion']; ?></p>
-                                    <img src="" alt="" class="" style="">
-                                </div>
-                            </div>
-                            <?php if ($tema['tipo_tema'] == "publicacion") {?>
-                                <?php foreach ($comentarios as $comentario) { ?>
-                                    <?php if ($comentario['id_tema'] == $tema['id_tema']) { ?>
-                                        <div class="card-footer text-muted">
-                                            <div class="media border p-3 bg-light comentario" id="<?php echo $comentario['id_tema']; ?>">
-                                                <img src="view/img/logogobierno.png" alt="John Doe" class="mr-3 mt-1 rounded-circle" style="width:60px;">
-                                                <div class="media-body">
-                                                    <h4><?php echo $comentario['p_nombre']." ".$comentario['apellido_p']?><small><i> Publicado el <?php echo $tema['fecha'] ?></i></small></h4>
-                                                    <p><?php echo $comentario['descripcion']; ?></p>
-                                                    <div class="media p-3">
-                                                    <img src="view/img/logogobierno.png" alt="Jane Doe" class="mr-3 mt-1 rounded-circle" style="width:45px;">
+                                <?php if ($tema['tipo_tema'] == "publicacion") {?>
+                                    <?php foreach ($comentarios as $comentario) { ?>
+                                        <?php if ($comentario['id_tema'] == $tema['id_tema']) { ?>
+                                            <div class="card-footer text-muted">
+                                                <div class="media border p-3 bg-light comentario" id="<?php echo $comentario['id_tema']; ?>">
+                                                    <img src="view/img/logogobierno.png" alt="John Doe" class="mr-3 mt-1 rounded-circle" style="width:60px;">
                                                     <div class="media-body">
-                                                        <h4>Jane Doe <small><i>Posted on February 20 2016</i></small></h4>
-                                                        <p>Lorem ipsum...</p>
+                                                        <h4><?php echo $comentario['p_nombre']." ".$comentario['apellido_p']?><small><i> Publicado el <?php echo $tema['fecha'] ?></i></small></h4>
+                                                        <p><?php echo $comentario['descripcion']; ?></p>
+                                                        <div class="media p-3">
+                                                        <img src="view/img/logogobierno.png" alt="Jane Doe" class="mr-3 mt-1 rounded-circle" style="width:45px;">
+                                                        <div class="media-body">
+                                                            <h4>Jane Doe <small><i>Posted on February 20 2016</i></small></h4>
+                                                            <p>Lorem ipsum...</p>
+                                                        </div>
+                                                        </div> 
                                                     </div>
-                                                    </div> 
-                                                </div>
-                                            </div>   
-                                        </div>
-                                    <?php } ?>  
-                                <?php } ?>
-
-                                <div class="media bg-white p-2 rounded-bottom">
-                                    <form action="index.php" method="post" class="w-100">
-                                        <div class="input-group mb-1 texto">
-                                            <input type="hidden" name="tema" value="<?php echo $tema['id_tema']; ?>">
-                                            <textarea name="descripcion" class="form-control texto bg-light" row="1" col="50" aria-label="With textarea"></textarea>
-                                            <div class="input-group-append">
-                                                <button name="comentar" value="comentar" class="btn btn-outline-primary font-weight-bold" type="submit" id="button-addon2">Comentar</button>
+                                                </div>   
                                             </div>
-                                        </div>
-                                    </form>
-                                </div>
-                            <?php } else if ($tema['tipo_tema'] == "encuesta") {?>
-                                <?php foreach ($opcion_encuestas as $opcion_encuesta) { ?>
-                                    <?php if ($opcion_encuesta['tema_id'] == $tema['id_tema']) { ?>
-                                        <div class="card-footer text-muted">
-                                            <div class="media border p-3 bg-light">
-                                                <form action="index.php" method="post">
-                                                    <?php if ((isset($encuestas[0])) && (in_array($_SESSION['dni'], $encuestas[0]))) {
+                                        <?php } ?>  
+                                    <?php } ?>
 
-                                                            echo "Usted ya ha participado en esta encuesta"; } 
+                                    <div class="media bg-white p-2 rounded-bottom">
+                                        <form action="index.php" method="post" class="w-100">
+                                            <div class="input-group mb-1 texto">
+                                                <input type="hidden" name="tema" value="<?php echo $tema['id_tema']; ?>">
+                                                <textarea name="descripcion" class="form-control texto bg-light" row="1" col="50" aria-label="With textarea"></textarea>
+                                                <div class="input-group-append">
+                                                    <button name="comentar" value="comentar" class="btn btn-outline-primary font-weight-bold" type="submit" id="button-addon2">Comentar</button>
+                                                </div>
+                                            </div>
+                                        </form>
+                                    </div>
+                                <?php } else if ($tema['tipo_tema'] == "encuesta") {?>
+                                    <?php foreach ($opcion_encuestas as $opcion_encuesta) { ?>
+                                        <?php if ($opcion_encuesta['tema_id'] == $tema['id_tema']) { ?>
+                                            <div class="card-footer text-muted">
+                                                <div class="media border p-3 bg-light">
+                                                    <form action="index.php" method="post">
+                                                        <?php if ((isset($encuestas[0])) && (in_array($_SESSION['dni'], $encuestas[0]))) {
 
-                                                        else { 
-                                                    ?>
-                                                        <input type="hidden" name="idtema" value="<?php echo $tema['id_tema'] ?>">
-                                                        <input type="hidden" name="usuario" value="<?php echo $_SESSION['dni']; ?>">
-                                                        <div class="form-check">
-                                                            <input class="form-check-input" type="radio" id="exampleRadios1" name="opcion" checked value="<?php echo $opcion_encuesta['opcion1'];?>">
-                                                            <label class="form-check-label" for="exampleRadios1">
-                                                                <?php echo $opcion_encuesta['opcion1'];?>
-                                                            </label>
-                                                        </div>
-                                                        <div class="form-check">
-                                                            <input class="form-check-input" type="radio" id="exampleRadios2" name="opcion" value="<?php echo $opcion_encuesta['opcion2'];?>">
-                                                            <label class="form-check-label" for="exampleRadios2">
-                                                                <?php echo $opcion_encuesta['opcion2'];?>
-                                                            </label>
-                                                        </div>
-                                                        <div class="form-check ">
-                                                            <input class="form-check-input" type="radio" id="exampleRadios3" name="opcion" value="<?php echo $opcion_encuesta['opcion3'];?>">
-                                                            <label class="form-check-label" for="exampleRadios3">
-                                                                <?php echo $opcion_encuesta['opcion3'];?>
-                                                            </label>
-                                                        </div>
-                                                    <?php } ?>
-                                                </div>
-                                                <div class="conteboton d-flex justify-content-center mt-3">
-                                                    <button type="submit" name="enviar" value="resencuesta" class="btn btn-primary">Responder</button>
-                                                </div>
-                                            </form>
-                                        </div>
-                                    <?php } ?>    
+                                                                echo "Usted ya ha participado en esta encuesta"; } 
+
+                                                            else { 
+                                                        ?>
+                                                            <input type="hidden" name="idtema" value="<?php echo $tema['id_tema'] ?>">
+                                                            <input type="hidden" name="usuario" value="<?php echo $_SESSION['dni']; ?>">
+                                                            <div class="form-check">
+                                                                <input class="form-check-input" type="radio" id="exampleRadios1" name="opcion" checked value="<?php echo $opcion_encuesta['opcion1'];?>">
+                                                                <label class="form-check-label" for="exampleRadios1">
+                                                                    <?php echo $opcion_encuesta['opcion1'];?>
+                                                                </label>
+                                                            </div>
+                                                            <div class="form-check">
+                                                                <input class="form-check-input" type="radio" id="exampleRadios2" name="opcion" value="<?php echo $opcion_encuesta['opcion2'];?>">
+                                                                <label class="form-check-label" for="exampleRadios2">
+                                                                    <?php echo $opcion_encuesta['opcion2'];?>
+                                                                </label>
+                                                            </div>
+                                                            <div class="form-check ">
+                                                                <input class="form-check-input" type="radio" id="exampleRadios3" name="opcion" value="<?php echo $opcion_encuesta['opcion3'];?>">
+                                                                <label class="form-check-label" for="exampleRadios3">
+                                                                    <?php echo $opcion_encuesta['opcion3'];?>
+                                                                </label>
+                                                            </div>
+                                                        <?php } ?>
+                                                    </div>
+                                                    <div class="conteboton d-flex justify-content-center mt-3">
+                                                        <button type="submit" name="enviar" value="resencuesta" class="btn btn-primary">Responder</button>
+                                                    </div>
+                                                </form>
+                                            </div>
+                                        <?php } ?>    
+                                    <?php } ?>
                                 <?php } ?>
-                            <?php } ?>
-                        </div>
-                    </div>                       
+                            </div>
+                        </div>                       
+                    <?php } ?>
                 <?php } ?>
 
                 <!--  -->
